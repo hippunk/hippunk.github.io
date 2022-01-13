2a6a6780a49350f46adfb4b195fad59d977162b6
Hipp <arthur.ramolet@hotmail.fr>
2022-01-13 15:58:09 +0100

    recentrage des cartes, ajout fermeture quand click sur background.

--- custom_scripts.js
+++ custom_scripts.js
@@ -8,11 +8,14 @@ function w3_close() {
   document.getElementById("mySidebar").style.display = "none";
 }
 
-function disable_scroll() {
+function open_popup(popup_name) {
+	document.getElementById(popup_name).style.display='block';
 	document.documentElement.style.overflow = 'hidden';
 }
 
-function enable_scroll() {
+function close_popup(popup_name) {
+	document.getElementById(popup_name).style.display='none';
+	document.querySelectorAll('video').forEach(vid => vid.pause());
 	document.documentElement.style.overflow = 'visible';
 }
 
@@ -20,22 +23,25 @@ function generate_card(title, thumbnail, description, content) {
 	var popup_name = "popup_"+title.replaceAll(" ","_").toLowerCase();
 
 	var template = `
-	<div onclick=\"document.getElementById('${popup_name}').style.display='block';disable_scroll();\" class=\"w3-btn card w3-round-large\"> 
+	<div onclick=\"open_popup('${popup_name}')\" class=\"w3-btn card w3-round-large\"> 
 		<h3>${title}</h3>
 		<img alt="Thumbnail du projet ${title}" class=\"w3-round-large\" src=${thumbnail} width=\"208\" height=\"167\"/>
 		<p class=\"card-text\">${description}</p>
 	</div>
-	<div id=${popup_name} class=\"w3-center w3-animate-opacity  w3-modal \">
-		<div class=\"w3-display-middle  w3-card-4 popup-limits\">
-		<header class=\"w3-container w3-color-dark\" style="position: sticky; top:0;"> 
-			<span onclick=\"document.getElementById('${popup_name}').style.display='none';document.querySelectorAll('video').forEach(vid => vid.pause());enable_scroll();\" class=\"w3-button w3-display-topright\">&times;</span>
-			<h2>${title}</h2>
-		</header>
-		<div class=\"w3-container w3-color-light\" >		
-			${content}
+	<div id=${popup_name} class=\"w3-center w3-animate-opacity  w3-modal\"  onclick=\"close_popup('${popup_name}');\" >
+		
+		<div class=\"w3-display-middle w3-card-4 popup-limits\" onclick=\"event.stopPropagation();\">
+			<header class=\"w3-container w3-color-dark\" style="position: sticky; top:0;" > 
+				<span onclick=\"close_popup('${popup_name}');\" class=\"w3-button w3-xlarge w3-display-topright\">&times;</span>
+				<h2>${title}</h2>
+			</header>
+			<div class=\"w3-container w3-color-light\" >		
+				${content}
+			</div>
 		</div>
-	  </div>
+		
 	</div>
 	`;
+	
 	return template;
 }
